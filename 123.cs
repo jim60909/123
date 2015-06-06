a3be448c4dce1b58472670dd6385b9be1839cf87
jim60909 <ryan50909@yahoo.com.tw>
2015-06-06 14:02:32 +0800

    ppp
    1323

--- 123.cs
+++ 123.cs
@@ -2,7 +2,7 @@
 {
     partial class MemberDetail
     {
-        /// <summary>
+        /// <
         /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
